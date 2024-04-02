200a42d133de5007c9c8605dc58dd33764d4aa7e
rian <rianrgomes@gmail.com>
2024-04-02 10:36:04 -0300

    set schema.sql

---
+++ init/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "events" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "title" TEXT NOT NULL,
+  "details" TEXT,
+  "slug" TEXT NOT NULL,
+  "maximum_attendees" INTEGER
+);
+
+CREATE TABLE "attendees" (
+  "id" TEXT NOT NULL PRIMARY KEY
+  "name" TEXT NOT NULL,
+  "email" TEXT NOT NULL,
+  "eventId" TEXT NOT NULL,
+  "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT "attendeesEventId" FOREIGN KEY ("eventId") REFERENCES 
+  "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE "checkIns" (
+  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+  "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "attendeeId" TEXT NOT NULL
+  CONSTRAINT "checkInsAttendeeId" FOREIGN KEY ("attendeeId") REFERENCES 
+  "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE UNIQUE INDEX "eventsSlugKey" ON "events"("slug");
+CREATE UNIQUE INDEX "attendeesEventIdEmail" ON "attendees"("eventId", "email");
+CREATE UNIQUE INDEX "checkInsAttendeeId" ON "checkIns"("attendeeId");
