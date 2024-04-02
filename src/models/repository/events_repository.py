57b04091b24991de12bd45362c04a81ee8aa3d00
rian <rianrgomes@gmail.com>
2024-04-02 18:48:17 -0300

    add get event by id function

--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,6 +1,7 @@
 from src.models.settings.connection import dbConnectionHandler
 from typing import Dict
 from src.models.entities.events import Events
+import json
 
 class EventsRepository:
   def addEvent(self, data: Dict) -> Dict:
@@ -15,3 +16,14 @@ class EventsRepository:
       database.session.add(event)
       database.session.commit()
       return data
+  
+  def getEventById(self, eventId: str) -> Dict:
+    with dbConnectionHandler as database:
+      response = database.session.query(Events).filter(Events.id==eventId).one()
+
+      return response
+
+  def getEvents(self):
+    with dbConnectionHandler as database:
+      response = database.session.query(Events)
+      return response
--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -3,14 +3,23 @@ from src.models.settings.connection import dbConnectionHandler
 
 dbConnectionHandler.connectDb()
 
-def test_addEvent():
+# def test_addEvent():
 
-  event = {
-    "uuid": "my-id-1-2",
-    "title": "event",
-    "slug": "event-01",
-    "maximumAttendees": 50
-  }
+#   event = {
+#     "uuid": "my-id-1-3",
+#     "title": "event",
+#     "slug": "event-02",
+#     "maximumAttendees": 50
+#   }
 
+#   eventsRepository = EventsRepository()
+#   print(eventsRepository.addEvent(event))
+
+def test_getEventById():
   eventsRepository = EventsRepository()
-  print(eventsRepository.addEvent(event))
+  print(eventsRepository.getEventById("my-id-1-3"))
+
+def test_getEvents():
+  eventsRepository = EventsRepository()
+  print(f"ajakjkasajsa:::: {eventsRepository.getEvents()}")
+    
